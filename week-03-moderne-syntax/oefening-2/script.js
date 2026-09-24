740e549868b3e8d94f6fdb0efd3554e7c4fdcbea
thymen69 <fieldies23@gmail.com>
2026-09-24 10:54:36 +0200

    opdracht gemaakt

--- week-03-moderne-syntax/oefening-2/script.js
+++ week-03-moderne-syntax/oefening-2/script.js
@@ -2,3 +2,20 @@
 // Knop 1: voeg tekst toe aan #message
 // Knop 2: voeg een <li> toe aan #list met een tekst
 // Knop 3: wissel de klasse 'active' op #message
+
+document.getElementById('btn-1').addEventListener('click', () => {
+    const message = document.getElementById('message');
+    message.textContent = 'knop 1 geklikt';
+});
+
+document.getElementById('btn-2').addEventListener('click', () => {
+    const list = document.getElementById('list');
+    const listItem = document.createElement('li');
+    listItem.textContent = 'ding toegevoegd';
+    list.appendChild(listItem);
+});
+
+document.getElementById('btn-3').addEventListener('click', () => {
+    const message = document.getElementById('message');
+    message.classList.toggle('active');
+});
--- week-03-moderne-syntax/uitleg/script.js
+++ week-03-moderne-syntax/uitleg/script.js
@@ -20,3 +20,19 @@ let fruits = ["Appel", "Banaan", "Perzik"]
 for(let fruit of fruits) {
     getName.innerHTML += fruit + "<br>";
 }
+
+let title = document.getElementById('title');
+let button = document.getElementById('btn');
+let section = document.getElementById('section');
+
+button.addEventListener('click', () => {
+    title.textContent = "Ik heb geklikt";
+    title.classList.toggle('active');
+
+    const p = document.createElement('p');
+
+    p.textContent = 'Ik voeg een nieuwe paragraaf toe';
+
+    section.appendChild(p)
+
+})
